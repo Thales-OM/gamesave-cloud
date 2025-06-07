e4d9cac4fb01c91f9d205d459e6d041c2313a84a
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-07 17:54:55 +0300

    feat: :sparkles: Add Git repository initialization utils

--- src/git_utils.py
+++ src/git_utils.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 import platform
+from src.constraints import DEFAULT_MASTER_BRANCH
 from src.logger import LoggerFactory
 
 
@@ -11,25 +12,60 @@ logger = LoggerFactory.getLogger(__name__)
 def is_git_installed() -> bool:
     try:
         # Check if git is available
-        subprocess.run(['git', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(
+            ["git", "--version"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         return True
-    except Exception:
+    except subprocess.CalledProcessError:
         return False
+
 
 def install_git() -> None:
     system = platform.system()
     if system == "Windows":
         # For Windows, you can use Chocolatey to install Git
         logger.info("Installing Git on Windows...")
-        subprocess.run(['winget', 'install', '--id', 'Git.Git', '--silent'], check=True)
+        subprocess.run(["winget", "install", "--id", "Git.Git", "--silent"], check=True)
     elif system == "Linux":
         # For Debian-based systems
         logger.info("Installing Git on Linux...")
-        subprocess.run(['apt-get', 'update', '-y'], check=True)
-        subprocess.run(['apt-get', 'install', 'git', '-y'], check=True)
+        subprocess.run(["apt-get", "update", "-y"], check=True)
+        subprocess.run(["apt-get", "install", "git", "-y"], check=True)
     elif system == "Darwin":
         # For macOS, you can use Homebrew
         logger.info("Installing Git on macOS...")
-        subprocess.run(['brew', 'install', 'git', '--quiet', '--force'], check=True)
+        subprocess.run(["brew", "install", "git", "--quiet", "--force"], check=True)
     else:
         raise OSError("Unsupported operating system. Please install Git manually.")
+
+
+def check_git_repository(path: str) -> bool:
+    # Check if the directory exists
+    if not os.path.isdir(path):
+        raise FileNotFoundError(f"Expected local repository path {path} does not exist.")
+    # Check if it's a git repository
+    try:
+        subprocess.run(
+            ["git", "-C", path, "rev-parse", "--is-inside-work-tree"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def create_git_repository(path: str) -> None:
+    # Create a new git repository
+    subprocess.run(["git", "-C", path, "init"], check=True)
+    logger.info(f"Created a new git repository at {path}.")
+
+
+def create_master_branch(path: str, master_branch: str = DEFAULT_MASTER_BRANCH) -> None:
+    # Create a master branch from the current HEAD
+    subprocess.run(["git", "-C", path, "checkout", "-b", master_branch], check=True)
+    logger.info("Created a master branch from the current HEAD.")
--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,26 @@
-from pydantic import Field, model_validator
+from pydantic import Field, model_validator, DirectoryPath
 from pydantic_settings import BaseSettings
 from typing import Optional, Union, Literal
-from src.constraints import DEFAULT_LOG_LEVEL
+from src.constraints import (
+    DEFAULT_LOG_LEVEL,
+    DEFAULT_LIMIT_SAVE_INTERVALS,
+    DEFAULT_SAVE_COOLDOWN_SEC,
+    DEFAULT_MASTER_BRANCH,
+    METADATA_STORAGE_FILEPATH,
+)
+
+
+class SaveStateSettings(BaseSettings):
+    limit_save_intervals: bool = DEFAULT_LIMIT_SAVE_INTERVALS
+    save_cooldown_sec: int = DEFAULT_SAVE_COOLDOWN_SEC
+
+
+class MetadataSettings(BaseSettings):
+    storage_filepath: DirectoryPath = METADATA_STORAGE_FILEPATH
+
+
+class GitSettings(BaseSettings):
+    master_branch: str = DEFAULT_MASTER_BRANCH
 
 
 class LoggingSettings(BaseSettings):
@@ -9,12 +28,26 @@ class LoggingSettings(BaseSettings):
 
     @model_validator(mode="after")
     def validate_log_level(self):
-        valid_levels = ('CRITICAL', 'FATAL', 'ERROR', 'WARN', 'WARNING', 'INFO', 'DEBUG', 'NOTSET')
+        valid_levels = (
+            "CRITICAL",
+            "FATAL",
+            "ERROR",
+            "WARN",
+            "WARNING",
+            "INFO",
+            "DEBUG",
+            "NOTSET",
+        )
         if self.log_level not in valid_levels:
             raise ValueError(f"Invalid log level. Must be one of {valid_levels}")
         return self
 
+
 class Settings(BaseSettings):
+    save_state: SaveStateSettings = SaveStateSettings()
+    metadata: MetadataSettings = MetadataSettings()
+    git: GitSettings = GitSettings()
     logging: LoggingSettings = LoggingSettings()
+
 
 settings = Settings()
