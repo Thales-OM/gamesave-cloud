34f5bd8117dab617d6af6589c11d5bc32c474afd
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-15 16:49:42 +0300

    feat: :sparkles: Add Controller status

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,5 @@
 from typing import List
+from enum import Enum
 from watchdog.observers import Observer
 from watchdog.observers.api import BaseObserver
 from src.models.tracked_directory import TrackedDirectory
@@ -9,9 +10,18 @@ from src.logger import LoggerFactory
 logger = LoggerFactory.getLogger(__name__)
 
 
+class Status(Enum):
+    NOT_INITIALIZED = "not_initialized"
+    STARTING = "starting"
+    STARTED = "started"
+    STOPPING = "stopping"
+    STOPPED = "stopped"
+
+
 class DirectoryController:
     directories: List[TrackedDirectory]
     observers: List[BaseObserver]
+    status: Status = Status.NOT_INITIALIZED
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "_instance"):
@@ -19,11 +29,15 @@ class DirectoryController:
         return cls._instance
 
     def __init__(self, directories: List[TrackedDirectory]):
+        self.status = Status.STARTING
+
         self.directories: List[TrackedDirectory] = []
         self.observers: List[BaseObserver] = []
 
         for directory in directories:
             self.add_directory(dir=directory)
+
+        self.status = Status.STARTED
 
     def add_directory(self, dir: TrackedDirectory) -> None:
         """Start watching a new directory"""
@@ -39,5 +53,9 @@ class DirectoryController:
         logger.info(f"Started watching directory: {path}")
 
     def stop_all(self) -> None:
+        self.status = Status.STOPPING
+
         for observer in self.observers:
             observer.stop()
+
+        self.status = Status.STOPPED
