4844f93e696b4cc399b6019e7b142fa6577d9990
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-15 04:09:05 +0300

    feat: :sparkles: Add settings for daemon process

--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,19 @@
 from pydantic import Field, model_validator, DirectoryPath
 from pydantic_settings import BaseSettings
-from typing import Optional, Union, Literal
 from src.constraints import (
     DEFAULT_LOG_LEVEL,
     DEFAULT_LIMIT_SAVE_INTERVALS,
     DEFAULT_SAVE_COOLDOWN_SEC,
     DEFAULT_MASTER_BRANCH,
     METADATA_STORAGE_FILEPATH,
+    DAEMON_PORT_RANGE_MIN,
+    DAEMON_PORT_RANGE_MAX,
 )
+
+
+class DaemonSettings(BaseSettings):
+    port_range_min: int = DAEMON_PORT_RANGE_MIN
+    port_range_max: int = DAEMON_PORT_RANGE_MAX
 
 
 class SaveStateSettings(BaseSettings):
@@ -39,11 +45,15 @@ class LoggingSettings(BaseSettings):
             "NOTSET",
         )
         if self.log_level not in valid_levels:
-            raise ValueError(f"Invalid log level. Must be one of {valid_levels}")
+            raise ValueError(
+                f"Invalid log level. \
+                             Must be one of {valid_levels}"
+            )
         return self
 
 
 class Settings(BaseSettings):
+    daemon: DaemonSettings = DaemonSettings()
     save_state: SaveStateSettings = SaveStateSettings()
     metadata: MetadataSettings = MetadataSettings()
     git: GitSettings = GitSettings()
