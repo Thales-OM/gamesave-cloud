794aeaa0c6c704c8716df256c1de70d064f6bd98
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-15 18:26:15 +0300

    feat: :sparkles: Prohibit last_save_time without a timezone

--- src/models/tracked_directory.py
+++ src/models/tracked_directory.py
@@ -1,9 +1,23 @@
-from pydantic import BaseModel, DirectoryPath
+from pydantic import BaseModel, DirectoryPath, field_validator
 from typing import Optional
 from datetime import datetime
+from src.logger import LoggerFactory
+
+
+logger = LoggerFactory.getLogger(__name__)
 
 
 class TrackedDirectory(BaseModel):
     name: str
     path: DirectoryPath
-    last_save_time_str: Optional[datetime] = None
+    last_save_time: Optional[datetime] = None
+
+    @field_validator("last_save_time_str", mode="after")
+    def verify_timezone(self, value: datetime) -> Optional[datetime]:
+        if value.tzinfo is None:
+            logger.warning(
+                "Got a last_save_time without a timezone, \
+                    setting value to None"
+            )
+            return None
+        return value
