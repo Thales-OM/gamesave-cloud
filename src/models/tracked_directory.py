299a3a5ed0c71e74b1dcabe942d0970067e7ea99
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-15 18:09:39 +0300

    fix: :bug: Allow last save timestamp to be None

--- src/models/tracked_directory.py
+++ src/models/tracked_directory.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, DirectoryPath
+from typing import Optional
 from datetime import datetime
 
 
 class TrackedDirectory(BaseModel):
     name: str
     path: DirectoryPath
-    last_save_time_str: datetime
+    last_save_time_str: Optional[datetime] = None
