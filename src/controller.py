c6c93b65cb0af380f02a3677649c25cee845c509
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-15 18:00:00 +0300

    fix: :safety_vest: Fix stopping cycle; make object control safer

--- src/controller.py
+++ src/controller.py
@@ -1,9 +1,10 @@
-from typing import List
+from typing import List, Dict, Optional
 from enum import Enum
 from watchdog.observers import Observer
 from watchdog.observers.api import BaseObserver
 from src.models.tracked_directory import TrackedDirectory
 from src.event_handler import TrackedDirectoryHandler
+from src.exceptions import ControllerCallError
 from src.logger import LoggerFactory
 
 
@@ -18,9 +19,19 @@ class Status(Enum):
     STOPPED = "stopped"
 
 
+class ControlPair:
+    directory: TrackedDirectory
+    observer: Optional[BaseObserver] = None
+
+    def __init__(
+        self, dir: TrackedDirectory, obs: Optional[BaseObserver] = None
+    ):
+        self.directory = dir
+        self.observer = obs
+
+
 class DirectoryController:
-    directories: List[TrackedDirectory]
-    observers: List[BaseObserver]
+    directories: Dict[str, ControlPair]
     status: Status = Status.NOT_INITIALIZED
 
     def __new__(cls, *args, **kwargs):
@@ -31,8 +42,7 @@ class DirectoryController:
     def __init__(self, directories: List[TrackedDirectory]):
         self.status = Status.STARTING
 
-        self.directories: List[TrackedDirectory] = []
-        self.observers: List[BaseObserver] = []
+        self.directories: Dict[str, ControlPair] = dict()
 
         for directory in directories:
             self.add_directory(dir=directory)
@@ -41,21 +51,47 @@ class DirectoryController:
 
     def add_directory(self, dir: TrackedDirectory) -> None:
         """Start watching a new directory"""
+        if dir.path in self.directories:
+            logger.warning(
+                "Tried adding an already present directory to the Controller"
+            )
+            return
+
+        self.directories[dir.path] = ControlPair(dir=dir)
 
         observer = Observer()
-        path = dir.path
         event_handler = TrackedDirectoryHandler()
 
-        observer.schedule(event_handler, path, recursive=True)
+        observer.schedule(event_handler, dir.path, recursive=True)
         observer.start()
-        self.observers.append(observer)
+        self.directories[dir.path].observer = observer
 
-        logger.info(f"Started watching directory: {path}")
+        logger.info(f"Started watching directory: {dir.path}")
 
     def stop_all(self) -> None:
+        if self.status == Status.STOPPING:
+            raise ControllerCallError(
+                "Tried stopping Controller when already being stopped"
+            )
+        if self.status == Status.STOPPED:
+            raise ControllerCallError(
+                "Tried stopping Controller when already stopped"
+            )
+        if self.status == Status.STARTING:
+            raise ControllerCallError(
+                "Controller currently starting, cannot stop. \
+                    Close the application manually if needed"
+            )
+
         self.status = Status.STOPPING
 
-        for observer in self.observers:
-            observer.stop()
+        while self.directories:
+            dir_path, pair = self.directories.popitem()
+            if pair.observer:
+                pair.observer.stop()
+                pair.observer.join()
+                logger.debug(f"Stopped observer for: {dir_path}")
+
+        logger.info("All directory watchers have been stopped and removed")
 
         self.status = Status.STOPPED
