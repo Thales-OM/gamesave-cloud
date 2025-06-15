7f896ef2bd9d194d3570281bf7c94e73306a5b44
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-15 18:27:02 +0300

    feat: :sparkles: Add ControllerCallError

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,3 +2,9 @@ class AvailablePortNotFoundError(BaseException):
     """
     No available port was found to host the daemon process server
     """
+
+
+class ControllerCallError(BaseException):
+    """
+    An invalid request was made to the Controller
+    """
