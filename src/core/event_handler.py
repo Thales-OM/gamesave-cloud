7bc9daa29fe750be066b079017af5b979e7d0692
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-24 16:55:39 +0300

    feat: :loud_sound: Add custom logger to event_handler.py

--- src/core/event_handler.py
+++ src/core/event_handler.py
@@ -13,20 +13,20 @@ class TrackedDirectoryHandler(FileSystemEventHandler):
 
     def on_modified(self, event):
         if not event.is_directory:
-            logger.info(f"Modified: {event.src_path}")
+            logger.debug(f"Modified: {event.src_path}")
             # Implement your action here
 
     def on_created(self, event):
         if not event.is_directory:
-            logger.info(f"Created: {event.src_path}")
+            logger.debug(f"Created: {event.src_path}")
             # Implement your action here
 
     def on_deleted(self, event):
         if not event.is_directory:
-            logger.info(f"Deleted: {event.src_path}")
+            logger.debug(f"Deleted: {event.src_path}")
             # Implement your action here
 
     def on_moved(self, event):
         if not event.is_directory:
-            logger.info(f"Moved: {event.src_path} to {event.dest_path}")
+            logger.debug(f"Moved: {event.src_path} to {event.dest_path}")
             # Implement your action here
