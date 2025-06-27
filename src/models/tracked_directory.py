85f8145b5be6ab20112b044092b7dc01654de3ab
pugin.ilya <pugin.ilya@rwb.ru>
2025-06-27 16:05:52 +0300

    feat: :sparkles: Add automatic name generation option for directory

--- src/models/tracked_directory.py
+++ src/models/tracked_directory.py
@@ -10,7 +10,7 @@ logger = LoggerFactory.getLogger(__name__)
 
 
 class TrackedDirectory(BaseModel):
-    name: str
+    name: Optional[str]
     path: DirectoryPath
     last_save_time: Optional[datetime] = None
 
@@ -27,5 +27,17 @@ class TrackedDirectory(BaseModel):
 
     @field_validator("path", mode="after")
     @classmethod
-    def make_path_absolute(cls, value) -> DirectoryPath:
+    def make_path_absolute(cls, value: DirectoryPath) -> DirectoryPath:
         return Path(os.path.abspath(value))
+
+    @staticmethod
+    def create_name_auto(path: str) -> str:
+        return os.path.basename(path)
+
+    @field_validator("name", mode="after")
+    def make_path_absolute(self, value: Optional[str]) -> str:
+        if value:
+            return value
+        synth_name = TrackedDirectory.create_name_auto(path=self.path)
+        logger.info(f"No name was provided for directory at path: {self.path}. Automatically assigning: {synth_name}.")
+        return synth_name
